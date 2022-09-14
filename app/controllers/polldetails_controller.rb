a33fcbab615547b16d378454e929c13eade66232
anumanu1995 <anubaby581995@gmail.com>
2022-09-14 15:01:00 +0530

    create polls

--- app/controllers/polldetails_controller.rb
+++ app/controllers/polldetails_controller.rb
@@ -2,12 +2,23 @@ class PolldetailsController < ApplicationController
     before_action :authenticate_user!
     layout 'admin'
     def new
+        @polldetail = Polldetail.new
+        @candidates = Candidate.left_joins(:polldetails).select('candidates.*').where(polldetails: {Candidate_id: nil})
+        @pollquestion = Pollcriterium.first    
+    end
+    def create
         @candidates = Candidate.left_joins(:polldetails).select('candidates.*').where(polldetails: {Candidate_id: nil})
         @pollquestion = Pollcriterium.first
-        #  @pollquestion.polloptions.each do |options|
-        #     puts options.poll_option
-        # end
-        
+        @polldetail = Polldetail.new(polldetail_params)
+        if @polldetail.save
+            redirect_to root_path
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+    private def polldetail_params
+        params[:polldetail][:Pollcriterium_id] = Pollcriterium.first.id
+        params.required(:polldetail).permit(:Pollcriterium_id,:Candidate_id,:Polloption_id)
         
     end
 end
--- app/models/polldetail.rb
+++ app/models/polldetail.rb
@@ -2,4 +2,6 @@ class Polldetail < ApplicationRecord
   belongs_to :Pollcriterium
   belongs_to :Candidate
   belongs_to :Polloption
+  validates :Candidate_id, presence: {:message=>nil}
+  validates :Polloption_id, presence: {:message=>nil}
 end
