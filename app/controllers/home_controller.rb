fbab79f25f7110bd0fba9f6c8435f8e75b2a2dc1
anumanu1995 <anubaby581995@gmail.com>
2022-09-15 12:21:05 +0530

    polldetails results

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,11 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
   layout 'admin'
   def index
+    @pollquestion = Pollcriterium.first
+    @candidates = Candidate.joins("INNER JOIN polldetails ON polldetails.Candidate_id = candidates.id INNER JOIN polloptions ON polloptions.id = polldetails.Polloption_id").select('(candidates.candidate_fname || " " || candidates.candidate_lname) as name,polloptions.poll_option,polldetails.id')
+    @polloptions = Polloption.all
+    @candidate_count = Candidate.all.count
+    @poll_count = Polldetail.all.count
+
   end
 end
--- app/controllers/polldetails_controller.rb
+++ app/controllers/polldetails_controller.rb
@@ -1,14 +1,19 @@
 class PolldetailsController < ApplicationController
     before_action :authenticate_user!
     layout 'admin'
+    def index
+        @candidates = Candidate.joins("INNER JOIN polldetails ON polldetails.Candidate_id = candidates.id INNER JOIN polloptions ON polloptions.id = polldetails.Polloption_id").select('(candidates.candidate_fname || " " || candidates.candidate_lname) as name,polloptions.poll_option,polldetails.id')
+    end
     def new
         @polldetail = Polldetail.new
         @candidates = Candidate.left_joins(:polldetails).select('candidates.*').where(polldetails: {Candidate_id: nil})
-        @pollquestion = Pollcriterium.first    
+        @pollquestion = Pollcriterium.first
+        @@candidates = @candidates
+        @@pollquestion= @pollquestion
     end
     def create
-        @candidates = Candidate.left_joins(:polldetails).select('candidates.*').where(polldetails: {Candidate_id: nil})
-        @pollquestion = Pollcriterium.first
+        @candidates =@@candidates
+        @pollquestion = @@pollquestion
         @polldetail = Polldetail.new(polldetail_params)
         if @polldetail.save
             redirect_to root_path
@@ -16,6 +21,11 @@ class PolldetailsController < ApplicationController
             render :new, status: :unprocessable_entity
         end
     end
+     def destroy
+        @polldetail = Polldetail.find(params[:id])
+        @polldetail.destroy
+        redirect_to polldetails_path
+     end
     private def polldetail_params
         params[:polldetail][:Pollcriterium_id] = Pollcriterium.first.id
         params.required(:polldetail).permit(:Pollcriterium_id,:Candidate_id,:Polloption_id)
